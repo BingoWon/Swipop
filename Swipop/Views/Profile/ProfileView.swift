1d4618c5f473db98d24402df1bd35790b8d66270
BingoW <bingow@outlook.com>
2025-12-05 16:37:45 +0800

    Fix profile page horizontal overflow
    - Add frame(width: screenWidth) to constrain swipeable grids container
    - Add clipped() to hide overflow content outside visible area
    - Fixes issue where all 3 tab grids were visible simultaneously

--- Swipop/Views/Profile/ProfileView.swift
+++ Swipop/Views/Profile/ProfileView.swift
@@ -121,6 +121,8 @@ struct ProfileContentView: View {
             }
         }
         .offset(x: -CGFloat(selectedTab) * screenWidth + dragOffset)
+        .frame(width: screenWidth, alignment: .leading)
+        .clipped()
         .gesture(
             DragGesture()
                 .onChanged { value in
